3a5e050ec5acccecae3e9749be2fd39782fa248f
sam bacha <sam@freighttrust.com>
2021-03-01 12:17:52 -0800

    Update utils.ts

--- server/utils.ts
+++ server/utils.ts
@@ -1,3 +1,6 @@
+// @file utils
+// @exports writeError
+// @param statusCode
 module.exports.writeError = (res: any, statusCode: any, errMsg: any) => {
   res.status(statusCode)
   res.json({ error: { message: errMsg } })
